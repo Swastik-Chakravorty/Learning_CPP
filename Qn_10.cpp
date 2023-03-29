326b548d726b60234751a21dc442cdbfcf574e4f
Swastik Chakravorty <swastik.chakravorty@gmail.com>
2023-03-29 16:04:59 +0530

    solved more problems

---
+++ Qn_10.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    double pi = 3.14159265;
+    cout << fixed << setprecision(4);
+    cout << "The value of pi : " << pi << endl;
+    cout << "The value of pi 4 decimal place of total width 8 : |" << setw(8) << pi << "|" << endl;
+    cout << "The value of pi 4 decimal place of total width 10 : |" << setw(10) << pi << "|" << endl;
+    cout << "The value of pi 4 decimal place of total width 8 : |" << setfill ('-') << setw(8) << pi << "|" << endl;
+    cout << "The value of pi 4 decimal place of total width 10 : |" << setfill ('-') << setw(10) << pi << "|" << endl;
+    cout << "The value of pi in scientific format is : " << (float)pi << endl;
+
+    bool done = false;
+    cout <<" Status in number : " << done << endl;  
+    cout << boolalpha;
+    cout <<" Status in alphabet : " << done << endl;
+    cout << endl;
+
+    return 0;
+}
