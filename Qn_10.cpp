b488117d2aaa3d264a70e5d8293a301d8869a117
Swastik Chakravorty <swastik.chakravorty@gmail.com>
2023-03-29 22:51:47 +0530

    added comments

--- Qn_10.cpp
+++ Qn_10.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main()
 {
     double pi = 3.14159265;
-    cout << fixed << setprecision(4);
+    cout << fixed << setprecision(4); //It will print upto 4 decimals
     cout << "The value of pi : " << pi << endl;
-    cout << "The value of pi 4 decimal place of total width 8 : |" << setw(8) << pi << "|" << endl;
+    cout << "The value of pi 4 decimal place of total width 8 : |" << setw(8) << pi << "|" << endl; //it will take places for 8 digits / characters
     cout << "The value of pi 4 decimal place of total width 10 : |" << setw(10) << pi << "|" << endl;
     cout << "The value of pi 4 decimal place of total width 8 : |" << setfill ('-') << setw(8) << pi << "|" << endl;
     cout << "The value of pi 4 decimal place of total width 10 : |" << setfill ('-') << setw(10) << pi << "|" << endl;
