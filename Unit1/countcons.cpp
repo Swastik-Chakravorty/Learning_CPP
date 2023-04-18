f3181d4891ab0dbedcc4953c66642a0d5f3bfefd
Swastik Chakravorty <112097563+Swastik-Chakravorty@users.noreply.github.com>
2023-04-18 13:06:06 +0530

    practice

---
+++ Unit1/countcons.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int curVal = 0;
+    int val = 0;
+    if (cin >> curVal)
+    {
+        int cnt = 1;
+        while (cin >> val)
+        {
+            if (val == curVal)
+            {
+                ++cnt;
+            }
+            else {
+                cout << curVal << "occurs" << cnt << "times" << endl;
+                curVal = val;
+                cnt = 1;
+            }
+        }
+        cout << curVal << "occurs" << cnt << "times" << endl;
+        
+    }
+
+    return 0;
+}
