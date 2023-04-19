cdda911086c57bcfcc19803124f2de0c0f7aee6d
Swastik Chakravorty <112097563+Swastik-Chakravorty@users.noreply.github.com>
2023-04-19 11:51:47 +0530

    1 Exercise from unit 2 in C

---
+++ Unit2/ex1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char txt[50];
+    printf("Input the string: ");
+    gets(txt);
+    printf("The string you entered is: %s\n", txt);
+
+    return 0;
+}
