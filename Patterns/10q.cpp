76a4e92feaea503a340a4197782abf29f8ea3ab2
Swastik Chakravorty <112097563+Swastik-Chakravorty@users.noreply.github.com>
2023-04-28 23:20:02 +0530

    10 exercise of Patterns in C++

---
+++ Patterns/10q.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the rows: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < (n - 1 - i); j++) 
+        // for (int j = (n - 1 - i); j > 0; j--)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k < n; k++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
