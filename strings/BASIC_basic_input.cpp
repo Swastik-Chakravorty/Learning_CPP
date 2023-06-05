d315321f53765e17018c0c399b7dfea06bb3265f
Swastik Chakravorty <112097563+Swastik-Chakravorty@users.noreply.github.com>
2023-06-05 22:48:35 +0530

    Input in C++

---
+++ strings/BASIC_basic_input.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    //For single word / word with no spaces
+    string str;
+    cin >> str;
+    string str1(5, 'n');
+    string str2 = "Swastikisgood";
+    cout << str << endl;
+    cout << str1 << endl;
+    cout << str2 << endl;
+    
+    return 0;
+}
