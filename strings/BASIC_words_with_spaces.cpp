edeed3da94e9c8762a849e740b5791dba0c41d6a
Swastik Chakravorty <112097563+Swastik-Chakravorty@users.noreply.github.com>
2023-06-05 22:53:52 +0530

    promt string with spaces in C++

---
+++ strings/BASIC_words_with_spaces.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    // For sentence with spaces
+    string str;
+    getline(cin, str);
+    cout << str << endl;
+
+    return 0;
+}
