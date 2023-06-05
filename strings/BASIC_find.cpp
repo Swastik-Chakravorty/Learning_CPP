1cc0669b5b78d417dd43e5b2987144b7de019c1f
Swastik Chakravorty <112097563+Swastik-Chakravorty@users.noreply.github.com>
2023-06-05 22:50:35 +0530

    find in string in C++

---
+++ strings/BASIC_find.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    string s1 = "nincompoop is a very serious problem";
+
+    cout << s1.find("very") << endl; //will display the starting position of the sub array
+    return 0;
+}
