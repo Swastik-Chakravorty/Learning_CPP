4efe2dcb03d748c19bb2d40a6b2baa156283fc4a
Swastik Chakravorty <112097563+Swastik-Chakravorty@users.noreply.github.com>
2023-06-22 23:05:22 +0530

    Intro to OOPs in C++

---
+++ OOP/intro.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include "hero.cpp"
+
+using namespace std;
+
+// class Hero {
+//     //properties
+//     // char name[100];
+//     // int health;
+//     // char level;
+// };
+
+int main()
+{
+    //creating a object
+    Hero h1;
+    cout << "size: " << sizeof(h1) << endl;
+
+
+    return 0;
+}
