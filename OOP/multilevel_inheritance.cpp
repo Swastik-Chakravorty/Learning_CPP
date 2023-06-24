01731dc334225bf667f79ae9f0767bb04040545d
Swastik Chakravorty <112097563+Swastik-Chakravorty@users.noreply.github.com>
2023-06-24 23:24:37 +0530

    Multilevel type inheritance in C++

---
+++ OOP/multilevel_inheritance.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+class Animal {
+    public:
+        int age;
+        int weight;
+
+    public:
+        void speak(){
+            cout << "Speaking" << endl;
+        }
+};
+
+class Dog : public Animal{
+
+};
+
+class GermanShepherd : public Dog{
+
+};
+
+int main()
+{
+    GermanShepherd doggy;
+    doggy.speak();
+    cout << doggy.age << endl;
+
+    return 0;
+}
